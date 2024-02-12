2daf4c67720aef204675f44458485df40c497cd3
JoseDanielCT <jdct0001@ce.pucmm.edu.do>
2024-02-12 03:11:51 -0400

    New branch

---
+++ src/main/java/edu/pucmm/pw/Main.java
@@ -0,0 +1,15 @@
+
+package edu.pucmm.pw;
+import io.javalin.Javalin;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    {
+        var app = Javalin.create(/*config*/)
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(7070);
+    }
+}
+ }
