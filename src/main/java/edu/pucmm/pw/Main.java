2f87ce7380c0b68a1a0e111b7866ed6694ed640d
JoseDanielCT <jdct0001@ce.pucmm.edu.do>
2024-02-12 03:18:17 -0400

    Commit de prueba

--- src/main/java/edu/pucmm/pw/Main.java
+++ src/main/java/edu/pucmm/pw/Main.java
@@ -4,7 +4,7 @@ import io.javalin.Javalin;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! NFL Chiefs Fan");
 
     {
         var app = Javalin.create(/*config*/)
